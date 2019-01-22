e155707812ca18e3222a08e38b49f158ce81c269
Marius Brad <mbrad26@gmail.com>
2019-01-22 10:46:32 +0000

    ..

--- app/errors.py
+++ app/errors.py
@@ -4,12 +4,11 @@ from app import app, db
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('404.html', title='Error 404'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
-
+    db.session.rollback
+    return render_template('500.html', title='Error 500'), 500
 
