19cc5f09cffb276ce7f5d4c126f1a99d5d122619
Marius Brad <mbrad26@gmail.com>
2019-02-02 12:09:24 +0000

    ..

--- app/models.py
+++ app/models.py
@@ -23,9 +23,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(60), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
     followed = db.relationship('User', secondary=followers,
                                primaryjoin=(followers.c.follower_id == id),
                                secondaryjoin=(followers.c.followed_id == id),
--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['mbrad_26@yahoo.com']
+    ADMINS = os.environ.get['ADMINS']
 
     POSTS_PER_PAGE = 5
 
