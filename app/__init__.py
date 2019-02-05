9c1646616349ece8895590ddc1da4db84c18203c
Marius Brad <mbrad26@gmail.com>
2019-02-05 14:27:00 +0000

    text search

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
+from elasticsearch import Elasticsearch
 
 
 app = Flask(__name__)
@@ -22,6 +23,8 @@ mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
+elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) if \
+    app.config['ELASTICSEARCH_URL'] else None
 
 
 from app.errors import bp as errors_bp
--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 import os
+from dotenv import load_dotenv
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_will_never_guess'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                                             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -23,3 +27,5 @@ class Config(object):
     LANGUAGES = ['es', 'en', 'ro', 'it']
 
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
